d85436cbfde5275d5c5df19d38381038d6acc105
Petro <thisnameisundefined@gmail.com>
2023-01-20 20:14:38 +0200

    Added env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import * as process from 'process';
+import { Logger } from '@nestjs/common';
 
-async function bootstrap() {
+const logger = new Logger('Main');
+const bootstrap = async (): Promise<void> => {
+  const PORT = process.env.API_PORT || 4001;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
-}
+  await app.listen(PORT);
+  logger.log(`The app started on the port ${PORT}`);
+};
+
 bootstrap();
