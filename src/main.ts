bf2ca836f91de24aa25d3777fbba7653e1180d90
Petro <thisnameisundefined@gmail.com>
2023-02-17 17:44:24 +0200

    Removed env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import * as process from 'process';
+// import * as process from 'process';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
 const logger = new Logger('Main');
 const bootstrap = async (): Promise<void> => {
-  const PORT = process.env.API_PORT || 4001;
+  const PORT = 4000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
