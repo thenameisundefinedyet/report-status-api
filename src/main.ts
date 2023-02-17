9d17f20c4bb59952a947943c5bb6d0f47a28b2bd
Petro <thisnameisundefined@gmail.com>
2023-02-17 18:37:13 +0200

    Changed port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 const logger = new Logger('Main');
 const bootstrap = async (): Promise<void> => {
-  const PORT = 4000;
+  const PORT = 8080;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
