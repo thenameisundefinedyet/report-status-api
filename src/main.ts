dba37c1e432f7d7122375a13f9a73644ec01a214
Petro <thisnameisundefined@gmail.com>
2023-02-17 18:57:27 +0200

    Changed port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(PORT);
+  await app.listen(process.env.PORT || 8080);
   logger.log(`The app started on the port ${PORT}`);
 };
 
