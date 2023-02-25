0d9146222b4712a389813d6293f548ba9bbfb08f
Petro <thisnameisundefined@gmail.com>
2023-02-25 17:34:01 +0200

    Fixed env

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 const logger = new Logger('Main');
 const bootstrap = async (): Promise<void> => {
-  const PORT = process.env.PORT || process.env.API_PORT || 8080;
+  const PORT = process.env.PORT || process.env.API_PORT;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
